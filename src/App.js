edb3f277b4ff70d87896b89dfb4bd9bd61f505d2
suryaaa01 <148421145+suryaaa01@users.noreply.github.com>
2023-10-31 11:42:10 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
-import React from 'react';
-import './SalesForm.css';
-import SalesForm from './SalesForm';
+// import "./App.css";
+// import MainDash from "./components/MainDash/MainDash";
+// import RightSide from "./components/RigtSide/RightSide";
+// import Sidebar from "./components/Sidebar";
+// import { BrowserRouter } from "react-router-dom";
+// // import {
+// //   createBrowserRouter,
+// //   RouterProvider,
+// // } from "react-router-dom";
+
+// function App() {
+//   return (
+//       <div className="App">
+//         <div className="AppGlass">
+//           <Sidebar />
+//           <MainDash />
+//           <RightSide />
+//         </div>
+//       </div>
+
+//   );
+// }
+
+// export default App;
+import './App.css'
+import MainContent from './components/MainContent/MainContent';
+import RightSide from './components/RigtSide/RightSide';
+import Sidebar from './components/Sidebar';
 
 function App() {
   return (
     <div className="App">
-      <SalesForm />
+      <div className="AppGlass">
+        <Sidebar/>
+        <MainContent/>
+        <RightSide/>
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,16 +1,59 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import ReactDOM from 'react-dom';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <App />,
+  </BrowserRouter>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// // import { BrowserRouter } from 'react-router-dom';
+// import App from './App';
+// // import {
+// //   createBrowserRouter,
+// //   RouterProvider,
+// // } from "react-router-dom";
+
+// // const router = createBrowserRouter([
+// //   {
+// //     path: "/",
+// //     element: <div>Hello world!</div>,
+// //   },
+// // ]);
+
+// ReactDOM.render(
+//     <App />,
+//   document.getElementById('root')
+// );
+
+// // ReactDOM.createRoot(document.getElementById("root")).render(
+// //   <React.StrictMode>
+// //     <RouterProvider router={router} />
+// //   </React.StrictMode>
+// // );
+
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
+
+// import {createRoot} from 'react-dom/client';
+// import App from './App';
+// import {BrowserRouter as Router} from 'react-router-dom';
+
+// const rootElement = document.getElementById('root');
+// const root = createRoot(rootElement);
+
+// // 👇️ wrap App in Router
+// root.render(
+//   <Router>
+//     <App />
+//   </Router>
+// );
